3d6eb625955c1cf67aa1f692020473498834220c
Mahfuj Islam <mahfujislam024@gmail.com>
2025-06-24 16:42:33 +0600

    add coffee update page

--- index.js
+++ index.js
@@ -33,11 +33,39 @@ async function run() {
             res.send(result)
         })
 
+        app.get("/coffees/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.findOne(query)
+            res.send(result)
+            console.log("details id : ", id)
+        })
+
         app.post("/coffees", async (req, res) => {
             const coffee = req.body
             const result = await coffeeCollection.insertOne(coffee)
             res.send(result)
             console.log("new cofee from client site :", coffee)
+        })
+
+        app.put("/coffees/:id", async (req, res) => {
+            const id = req.params.id
+            const coffee = req.body
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: coffee.name,
+                    chef: coffee.chef,
+                    supplier: coffee.supplier,
+                    taste: coffee.taste,
+                    category: coffee.category,
+                    details: coffee.details,
+                    photo: coffee.photo
+                }
+            }
+            const result = await coffeeCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
         })
 
         app.delete("/coffees/:id", async (req, res) => {
